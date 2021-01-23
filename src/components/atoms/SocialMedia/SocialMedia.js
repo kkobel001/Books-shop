2876bd8c9625157f7c9be3a80dace1ebfaa924a0
kkobel001 <katarzyna.kobel@vp.pl>
2021-01-23 14:03:14 +0100

    Finished Footer

---
+++ src/components/atoms/SocialMedia/SocialMedia.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+const StyledWrapper = styled.div`
+  height: 37px;
+  width: 100%px;
+  text-align: center;
+`;
+
+const Styledul = styled.ul`
+  height: 100%;
+  list-style-type: none;
+  text-align: center;
+  & li {
+    float: left;
+    height: 37px;
+    width: 37px;
+    margin-right: 3px;
+    background-color: #000000;
+    padding-top: 20px;
+    margin-right: 15px;
+  }
+`;
+
+const StyledFacebookIcon = styled(FacebookIcon)`
+  color: #ffffff;
+  font-size: 20px;
+  &:hover {
+    color: #bac237;
+  }
+`;
+const StyledTwitterIcon = styled(TwitterIcon)`
+  color: #ffffff;
+  font-size: 20px;
+  &:hover {
+    color: #bac237;
+  }
+`;
+const StyledWhatsAppIcon = styled(WhatsAppIcon)`
+  color: #ffffff;
+  font-size: 20px;
+  &:hover {
+    color: #bac237;
+  }
+`;
+const StyledInstagramIcon = styled(InstagramIcon)`
+  color: #ffffff;
+  font-size: 20px;
+  &:hover {
+    color: #bac237;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  height: 100%;
+`;
+
+const SocialMedia = () => (
+  <StyledWrapper>
+    <Styledul>
+      <li>
+        <StyledLink to="./">
+          <StyledFacebookIcon />
+        </StyledLink>
+      </li>
+      <li>
+        <StyledLink to="./">
+          <StyledTwitterIcon />
+        </StyledLink>
+      </li>
+      <li>
+        <StyledLink to="./">
+          <StyledWhatsAppIcon />
+        </StyledLink>
+      </li>
+      <li>
+        <StyledLink to="./">
+          <StyledFacebookIcon />
+        </StyledLink>
+      </li>
+      <li>
+        <StyledLink to="./">
+          <StyledInstagramIcon />
+        </StyledLink>
+      </li>
+    </Styledul>
+  </StyledWrapper>
+);
+
+export default SocialMedia;
--- src/components/molecules/Footer/Footer.js
+++ src/components/molecules/Footer/Footer.js
@@ -6,8 +6,7 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import { Container } from '../../../theme/GlobalStyle';
 import { Button } from '../../atoms/Button/Button';
 import { Input } from '../../atoms/Input/Input';
-
-// import  SocialMediaIcons  from '../../atoms/SocialMediaIcons/SocialMediaIcons';
+import SocialMedia from '../../atoms/SocialMedia/SocialMedia';
 
 const StyledContainer = styled(Container)`
   height: 100%;
@@ -47,10 +46,31 @@ const StyledWrapper = styled.div`
     max-width: 300px;
     line-height: 1.80857;
   }
+
+  @media screen and (max-width: 1290px) {
+    margin-left: auto;
+    margin-right: auto;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 720px) {
+    align-items: center;
+    grid-template-columns: repeat(1, 1fr);
+  }
 `;
 const CardInfo = styled.div`
   height: 380px;
   padding: 40px 0;
+
+  @media screen and (max-width: 1230px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media screen and (max-width: 720px) {
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+    height: 300px;
+  }
 `;
 const StyledNewsletter = styled.div`
   display: inline-block;
@@ -61,16 +81,24 @@ const StyledButton = styled(Button)`
 
 const InfoFooter = styled.div`
   text-align: center;
-  font-size: 14px;
-  background-color: #060606;
+  /* font-size: 14px; */
+  /* background-color: #060606; */
   width: 100%;
   height: 40px;
   color: #ffffff;
-  /* margin-left:0px;
-margin-right:-50px; */
-
+  margin-left: 0px;
+  margin-right: 0px;
+  ::before {
+    content: '';
+    border-bottom: 2px solid #b0b435;
+    display: block;
+    width: 100%;
+    bottom: 3px;
+    left: 0px;
+  }
   & p {
-    padding-top: 10px;
+    padding-top: 15px;
+    margin-bottom: 10px;
   }
 `;
 
@@ -83,10 +111,7 @@ const StyledInformation = styled.ul`
     color: #b0b435;
   }
 `;
-const StyledContact = styled.ul`
-  & p {
-  }
-`;
+const StyledContact = styled.ul``;
 
 const Footer = () => (
   <StyledContainer>
@@ -108,7 +133,7 @@ const Footer = () => (
       </CardInfo>
       <CardInfo>
         <h3>Social Media</h3>
-        {/* <SocialMediaIcons /> */}
+        <SocialMedia />
       </CardInfo>
 
       <CardInfo>
